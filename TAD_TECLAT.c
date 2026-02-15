00100fe31296fdac99f9e2ed1048c44aeb8ae1c7
Ariadnagn <91936920+Ariadnagn@users.noreply.github.com>
2026-02-15 17:48:40 +0100

    Teclat més entenedor

--- TAD_TECLAT.c
+++ TAD_TECLAT.c
@@ -7,62 +7,45 @@
 
 
 #include "TAD_TECLAT.h"
+#include "TAD_SERIAL.h"
 
 static unsigned char timerRebots;
 static unsigned char timerSMS;
 
-static unsigned char estatTeclat;
-static unsigned char filaActiva;
-
-static char lastChar;
+static char lastChar = '0';
 static unsigned char newChar;
+static char auxChar;
+static char teclaPremuda = 0;
 
-static char lastKey;
 static unsigned char pulsacions;
 
 static const char SMS_0[FILES][COLUMNES] = {
     {'1','2','3'},
     {'4','5','6'},
     {'7','8','9'},
-    {'0','0','0'}     
+    {'*','0','#'}     
 };
 
 static const char SMS_1[FILES][COLUMNES] = {
     {'1','A','D'},
     {'G','J','M'},
     {'P','T','W'},
-    {'0',' ','0'}
+    {'*','0','#'}
 };
 
 static const char SMS_2[FILES][COLUMNES] = {
     {'1','B','E'},
     {'H','K','N'},
     {'R','U','X'},
-    {'0','0','0'}
+    {'*','0','#'}
 };
 
 static const char SMS_3[FILES][COLUMNES] = {
     {'1','C','F'},
     {'I','L','O'},
     {'S','V','Y'},
-    {'0',' ','0'}
+    {'*','0','#'}
 };
-
-#define KEY_PRESSED   (!PORTBbits.RB1 || !PORTBbits.RB2 || !PORTBbits.RB3)
-
-static char DetectKey(unsigned char fila) {
-    if (!PORTBbits.RB1) return SMS_0[fila][0];
-    if (!PORTBbits.RB2) return SMS_0[fila][1];
-    if (!PORTBbits.RB3) return SMS_0[fila][2];
-    return 0;
-}
-
-static char SMS_Convert(unsigned char fila, unsigned char columna, unsigned char prem) {
-    if (prem == 1) return SMS_1[fila][columna];
-    if (prem == 2) return SMS_2[fila][columna];
-    if (prem == 3) return SMS_3[fila][columna];
-    return SMS_1[fila][columna];
-}
 
 void Init_Teclat(void) {
 
@@ -78,119 +61,170 @@ void Init_Teclat(void) {
     TRISBbits.RB3 = 1;
 
     INTCON2bits.RBPU = 0;
-    ADCON1 = 0x0F;
+    ADCON1 = 0xFF;
 
     TI_NewTimer(&timerRebots);
     TI_NewTimer(&timerSMS);
 
-    estatTeclat = 0;
-    filaActiva  = 0;
-
     lastChar = 0;
     newChar  = 0;
-
-    lastKey = 0;
+    
     pulsacions = 0;
 }
 
 void MotorTeclat(void) {
-
-    char key;
-    unsigned char col = 0;
+    static unsigned char estatTeclat = 0;
+    char fila;
+    unsigned char columna = 0;
 
     switch (estatTeclat) {
-        case 0: case 1: case 2: case 3:
-            LATAbits.LATA0 = (estatTeclat != 0);
-            LATAbits.LATA1 = (estatTeclat != 1);
-            LATAbits.LATA2 = (estatTeclat != 2);
-            LATAbits.LATA3 = (estatTeclat != 3);
-
-            filaActiva = estatTeclat;
-
-            if (KEY_PRESSED) {
-                TI_ResetTics(timerRebots);
+        case 0:
+            if(!PREMUT()) {
+                LATAbits.LATA0=1;
+                LATAbits.LATA1=0;
+                LATAbits.LATA2=0;
+                LATAbits.LATA3=0;
+                estatTeclat = 1;
+                fila = 0;
+            }
+            else {
                 estatTeclat = 4;
-            } else {
-                estatTeclat = (estatTeclat + 1) & 0x03;
             }
-        break;
-
-        /* ---------- Rebots ---------- */
+            break;
+            
+        case 1:
+            if(!PREMUT()) {
+                LATAbits.LATA0=0;
+                LATAbits.LATA1=1;
+                LATAbits.LATA2=0;
+                LATAbits.LATA3=0;
+                estatTeclat = 2;
+                fila = 1;
+            }
+            else {
+                estatTeclat = 4;
+            }
+            break;
+            
+        case 2:
+            if(!PREMUT()) {
+                LATAbits.LATA0=0;
+                LATAbits.LATA1=0;
+                LATAbits.LATA2=1;
+                LATAbits.LATA3=0;
+                estatTeclat = 3;
+                fila = 2;
+            }
+            else {
+                estatTeclat = 4;
+            }
+            break;
+            
+        case 3:
+            if(!PREMUT()) {
+                LATAbits.LATA0=0;
+                LATAbits.LATA1=0;
+                LATAbits.LATA2=0;
+                LATAbits.LATA3=1;
+                estatTeclat = 0;
+                fila = 3;
+            }
+            else {
+                estatTeclat = 4;
+            }
+            break;
+            
         case 4:
-            if (TI_GetTics(timerRebots) >= REBOT_TICS) {
-                if (KEY_PRESSED) estatTeclat = 5;
-                else estatTeclat = 0;
-            }
+            TI_ResetTics(timerRebots);
+            estatTeclat = 5;
         break;
-
-        /* ---------- Lectura Tecla ---------- */
+        
         case 5:
-            if (!PORTBbits.RB1) col = 0;
-            else if (!PORTBbits.RB2) col = 1;
-            else if (!PORTBbits.RB3) col = 2;
-
-            key = SMS_0[filaActiva][col];
-
-            if (key == lastKey) {
-                pulsacions++;
-                if (pulsacions > 3) pulsacions = 1;
-            } else {
-                pulsacions = 1;
-                lastKey = key;
-            }
-
-            lastChar = SMS_Convert(filaActiva, col, pulsacions);
-            newChar  = 0;
-
-            TI_ResetTics(timerSMS);
-            estatTeclat = 6;
-        break;
-
-        /* ---------- Espera ---------- */
-        case 6:
-            if (!KEY_PRESSED) {
-                TI_ResetTics(timerRebots);
-                estatTeclat = 7;
-            }
-        break;
-
-        /* ---------- Confirmaci� ---------- */
-        case 7:
-            if (TI_GetTics(timerRebots) >= REBOT_TICS) {
-                if (!KEY_PRESSED) {
-                    newChar = 1;
-                    estatTeclat = 8;
-                } else {
+            if(TI_GetTics(timerRebots) >= REBOT_TICS) {
+                if(PREMUT()) {
                     estatTeclat = 6;
+                }
+                else {
+                    estatTeclat = 0;
                 }
             }
         break;
-
-        /* ---------- Timeout SMS ---------- */
-        case 8:
-            if (TI_GetTics(timerSMS) >= SMS_TIMEOUT_TICS) {
-                lastKey = 0;
-                pulsacions = 0;
-                estatTeclat = 0;
-            } else if (KEY_PRESSED) {
-                estatTeclat = filaActiva;
+        
+        case 6:
+            if(PORTBbits.RB1){  
+                columna=0;  
+            } else if(PORTBbits.RB2){
+                columna=1;  
+            } else if(PORTBbits.RB3){
+                columna=2;  
             }
+   
+            lastChar = newChar;
+            newChar = getSMS_0(fila,columna);
+            auxChar = newChar;
+            teclaPremuda = 1;
+            
+            if (newChar == lastChar){
+                pulsacions++;
+                if (pulsacions == 1){
+                        auxChar = SMS_1[fila][columna];
+                } else if (pulsacions == 2){
+                    auxChar = SMS_2[fila][columna];
+                } else if (pulsacions == 3){
+                    auxChar = SMS_3[fila][columna];
+                    lastChar = 0;
+                } else if (pulsacions == 4){
+                    pulsacions = 0;
+                }
+                if (TXSTAbits.TRMT) {
+                    Serial_PutChar(auxChar);
+                }
+            }
+            else{
+                pulsacions = 0;
+                if (TXSTAbits.TRMT) {
+                    Serial_PutChar(auxChar);
+                }
+            }
+            estatTeclat=7;
         break;
+        
+        case 7:
+            if (!PREMUT()) {
+                TI_ResetTics(timerRebots);
+                estatTeclat=8;
+            }
+            break;
+            
+        case 8:
+            if (TI_GetTics(timerRebots) >= REBOT_TICS){
+                if(!PREMUT()) {
+                    TI_ResetTics(timerRebots);
+                    estatTeclat = 0;
+                }
+                else {
+                    estatTeclat = 7;
+                }
+            }
+            break;
     }
 }
 
-char Teclat_GetLastChar(void) {
-    newChar = 0;
-    return lastChar;
-}
+//char Teclat_GetLastChar(void) {
+//    newChar = 0;
+//    return lastChar;
+//}
+//
+//unsigned char Teclat_HasNewChar(void) {
+//    return newChar;
+//}
 
-unsigned char Teclat_HasNewChar(void) {
-    return newChar;
+char getSMS_0(char fila, char columna) {
+    return SMS_0[fila][columna];
 }
 
 void Teclat_Reset(void) {
     lastChar = 0;
     newChar = 0;
-    lastKey = 0;
     pulsacions = 0;
 }
