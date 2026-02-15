e0103f443d76b381ada195e78a8d5a836d929621
Tassani1 <tassvid@gmail.com>
2026-02-15 19:50:50 +0100

    Controller començat, falta fer lo dels 10 segons del protocol d'alarma i fer el procés de sortida si està bé.

--- TAD_CONTROLLER.c
+++ TAD_CONTROLLER.c
@@ -18,8 +18,16 @@
 #include "TAD_LEDS.h"
 //TODO: incloure .h del HALL
 
+
+
 static unsigned char Estat;
 static unsigned char timerController;
+static char intentsRestants = 3;
+static unsigned char hallActivat = 0;
+static unsigned char PIN[8];
+static unsigned char iPIN = 0;
+static unsigned char PIN_CORRECTE[] = "1511MTV"
+static unsigned char esCorrecte = 1;
 
 void Init_Controller(void){
     TI_NewTimer(&timerController);
@@ -27,12 +35,21 @@ void Init_Controller(void){
 
 void CO_HallDetected(void) {}
 
-void CO_NewKeyPressed(unsigned char key) {}
+
+
+void CO_NewKeyPressed(unsigned char key) {
+    //DDMMXXX + /0
+    if(iPIN <8){
+        PIN[iPIN++] = key;
+    }   
+
+}
 
 void CO_ExitRequested(void) {}
 
 void CO_motor(){
-    unsigned char hallActivat = 0;
+   
+
     switch(Estat){
         case 0: 
             //TODO: Funció del serial que printi el missatge de bevinguda
@@ -44,20 +61,66 @@ void CO_motor(){
         case 1:
             //TODO: posar un if per si es detecta el HALL
                 //dintre del if
-                //TODO: Funcio del serial que printi el missatge: >LSBank-Open exterior door
+                //TODO: Funcio del serial que printi el missatge: > LSBank - Open exterior door
                 //espera de 2 segons
                 hallActivat = 1;
 
                 if(hallActivat){
                     if(TI_GetTics(timerController)>= 1000){
-                    //TODO: Funcio del serial que printi el missatge: >LSBank-close/closed exterior door
-                    SPE_PlayAcuteSound();
-                    //TODO: Funcio del serial que printi el missatge: >LSBank_EnterPIN:
-                    Estat = 2;
-                    hallActivat = 0;
+                        //TODO: Funcio del serial que printi el missatge: > LSBank - close/closed exterior door
+                        SPE_PlayAcuteSound();
+                        //TODO: Funcio del serial que printi el missatge: > LSBank - Enter PIN:
+                        Estat = 2;
+                        hallActivat = 0;
+                        TI_ResetTics(timerController);
                     }
                 }
         case 2: 
+                
+                //comprovar si no han passat 2 minuts
+                if(TI_GetTics(timerController)<= 60000){
+                    esCorrecte = 1;
+                    SPE_PlayPressureSound();
+                    INT_start();
+                    if(iPIN == 7){
+                        PIN[8] = '\0';
+                        for(unsigned char i = 0; i<8 ; i++){
+                            if(PIN[i] != PIN_CORRECTE[i]){
+                                esCorrecte = 0;
+                                break;
+                            }                            
+                        }
+                    }
+                    iPIN = 0;
+                    if (!esCorrecte){
+                        if(--intentsRestants == 0){
+                            //ALARMA
+                            SPE_PlayAlarmSound();
+                            //TODO: Serial posasr: > LSBank - Thief Intercepted 
+                            LEDS_EncenLed (LED_STATE_ALARM);
+                            LEDS_ApagaLed (LED_STATE_OK);
+                            TI_ResetTics(timerController);
+                            INT_stop();
+                            Estat = 3;
+
+                        
+                        } else{
+
+                            //posar > LSBank - Permision Denied
+                            //posar > LSBank - Enter PIN
+                        }
+                        
+
+                        
+                    }
+
+                }
+                else{
+                    Estat = 3;
+                }
+        //estat alarma
+        case 3: 
+                
                 
             
                 
