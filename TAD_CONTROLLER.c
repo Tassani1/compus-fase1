ed2aa702aa721c0371e2f530da2c33e9a03beccc
Tassani1 <tassvid@gmail.com>
2026-02-16 09:40:04 +0100

    Controller amb lògica acabada(HI ha molts TODO)

--- TAD_CONTROLLER.c
+++ TAD_CONTROLLER.c
@@ -23,11 +23,17 @@
 static unsigned char Estat;
 static unsigned char timerController;
 static char intentsRestants = 3;
-static unsigned char hallActivat = 0;
 static unsigned char PIN[8];
 static unsigned char iPIN = 0;
-static unsigned char PIN_CORRECTE[] = "1511MTV"
 static unsigned char esCorrecte = 1;
+static unsigned char Static Alarma = 1;
+static unsigned char PIN_CORRECTE = 0; 
+
+static unsigned char PIN_CORRECTE[] = "1511MTV";
+static unsigned char SOLICITUD_ACCEPTADA[] = "Yes";
+static unsigned char SOLICITUD_DENEGADA[] = "No";
+
+
 
 void Init_Controller(void){
     TI_NewTimer(&timerController);
@@ -52,83 +58,141 @@ void CO_motor(){
 
     switch(Estat){
         case 0: 
-            //TODO: Funció del serial que printi el missatge de bevinguda
+            //TODO: Funció del serial que printi el missatge de bevinguda(No se si sha de posar nomes un cop en tot el programa o cada cop que es reseteja)
             LEDS_EncenLed(LED_STATE_OK);
+            LEDS_ApagaLed(LED_STATE_ALARM);
             TI_ResetTics(timerController);
+            //Per tenir disponible un altre cop el protocol d'alarma
+            Alarma = 1;
             Estat = 1;
             
             break;
+        //Estat d'espera del hall
         case 1:
             //TODO: posar un if per si es detecta el HALL
                 //dintre del if
                 //TODO: Funcio del serial que printi el missatge: > LSBank - Open exterior door
-                //espera de 2 segons
-                hallActivat = 1;
-
-                if(hallActivat){
-                    if(TI_GetTics(timerController)>= 1000){
+                TI_ResetTics(timerController);
+                Estat = 2;                
+                break;
+        //estat que espera dos segons un cop s'ha posat el hall
+        case 2:
+            if(TI_GetTics(timerController)>= 1000){
                         //TODO: Funcio del serial que printi el missatge: > LSBank - close/closed exterior door
                         SPE_PlayAcuteSound();
                         //TODO: Funcio del serial que printi el missatge: > LSBank - Enter PIN:
-                        Estat = 2;
-                        hallActivat = 0;
                         TI_ResetTics(timerController);
-                    }
-                }
-        case 2: 
-                
-                //comprovar si no han passat 2 minuts
-                if(TI_GetTics(timerController)<= 60000){
-                    esCorrecte = 1;
-                    SPE_PlayPressureSound();
-                    INT_start();
-                    if(iPIN == 7){
-                        PIN[8] = '\0';
-                        for(unsigned char i = 0; i<8 ; i++){
-                            if(PIN[i] != PIN_CORRECTE[i]){
-                                esCorrecte = 0;
-                                break;
-                            }                            
-                        }
-                    }
-                    iPIN = 0;
-                    if (!esCorrecte){
-                        if(--intentsRestants == 0){
-                            //ALARMA
-                            SPE_PlayAlarmSound();
-                            //TODO: Serial posasr: > LSBank - Thief Intercepted 
-                            LEDS_EncenLed (LED_STATE_ALARM);
-                            LEDS_ApagaLed (LED_STATE_OK);
-                            TI_ResetTics(timerController);
-                            INT_stop();
-                            Estat = 3;
-
-                        
-                        } else{
-
-                            //posar > LSBank - Permision Denied
-                            //posar > LSBank - Enter PIN
-                        }
-                        
-
-                        
-                    }
-
-                }
-                else{
-                    Estat = 3;
-                }
-        //estat alarma
+                        Estat = 3;
+            }
+            break; 
         case 3: 
                 
-                
-            
-                
+            //comprovar si no han passat 2 minuts
+            if(TI_GetTics(timerController)<= 60000){
+                esCorrecte = 1;
+                PIN_CORRECTE = 0;
+                SPE_PlayPressureSound();
+                INT_start();
+                if(iPIN == 7){
+                    PIN[8] = '\0';
+                    for(unsigned char i = 0; i<8 ; i++){
+                        if(PIN[i] != PIN_CORRECTE[i]){
+                            esCorrecte = 0;
+                            break;
+                        }                            
+                    }
+                }
+                iPIN = 0;
+                if (!esCorrecte){
+                    if(--intentsRestants == 0){
+                        //anira al protocol d'alarma
+                        Estat = 4;
+                    } else{
+
+                        //posar > LSBank - Permision Denied
+                        //posar > LSBank - Enter PIN
+                    }
+                    
 
                     
+                } else {
+                    //Si es correcte
+                    INT_stop();
+                    //TODO: Posar missatge de: > LSBank - Open Interior door
+                    TI_ResetTics(timerController);
+                    Estat = 5;
+
+                }
+
+            }
+            else{
+                //han passat els 2 minuts (ALARMA)
+                Estat = 4;
+            }
+            break;
+        //estat alarma
+        case 4: 
             
+            if(Alarma){
+                SPE_PlayAlarmSound();
+                //TODO: Serial posasr: > LSBank - Thief Intercepted 
+                LEDS_EncenLed (LED_STATE_ALARM);
+                LEDS_ApagaLed (LED_STATE_OK);
+                TI_ResetTics(timerController);
+                INT_stop();
+
+                Alarma = 0;
+            }
+            
+            if(TI_GetTics(timerController)<= 5000){
+                //TODO: Posar missatge pel serial de :  > LSBank - Reset System: 
+
+                //TODO: llegir la resposta amb el serial
+                //TODO: Posar if comprovant si el Missatge es "Yes"
+                    //Si es "Yes"
+                    //TI_ResetTics(timerController);
+                    //Anar al estat 0
+                    
+                //TODO: else 
+                    //TODO: Posar missatge pel serial de :  > LSBank - Reset System:
+
+            } else {
+                TI_ResetTics(timerController);
+                Estat = 0;
+            }
+            break;
+
+        //Espera de 2 segons quan es correcte
+        case 5: 
+            if(TI_GetTics(timerController)>= 1000){
+                //TODO: Printar > LSBank - Closed interior door
+                SPE_PlayAcuteSound();
+                Estat = 6; 
+            }
+            break;
+        //Espera del botó per sortir
+        case 6:
+            if(CO_ExitRequested()){
+                //TODO: printar: > LSBank - Exit Requested:
+
+                //if "Yes"
+                    //TODO: printar: > LSBank - Open bot doors
+                    TI_ResetTics(timerController);
+                    Estat = 7;
+                //else ("No")
+                    //Estat = 4;
 
 
+            }
+
+            break;
+        //espera de 2 segons quan s'ha acceptat la sortida del bank
+        case 7: 
+            if(TI_GetTics(timerController)>= 1000){
+                //TODO: printar > LSBank - Close both doors
+                SPE_PlayAcuteSound();
+                Estat = 0;
+            }
     }
 }
 
